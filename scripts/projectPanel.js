5fde6ad02abab4f6bc76b1143e762f84b847930f
dumiswa <dumitrudumi605@yahoo.com>
2025-05-10 13:13:07 +0200

    project presentations

--- scripts/projectPanel.js
+++ scripts/projectPanel.js
@@ -7,34 +7,25 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
     const projectData = {
-        "Project 1": {
-            title: "Project 1",
-            description: "Description for Project 1",
-        },
-        "Project 2": {
-            title: "Project 2",
-            description: "Description for Project 2",
-        },
-        // Add more projects
-
+    "Project 1": { title: "Project 1", url: "Projects/project1.html" },
+    "Project 2": { title: "Project 2", url: "Projects/project2.html" },
+    "Project 3": { title: "Project 3", url: "Projects/project3.html" },
+    "Project 4": { title: "Project 4", url: "Projects/project4.html" }
     };
 
     cards.forEach(card => {
         card.addEventListener("click", () => {
-            const projectName = card.querySelector("h3").innerText;
+        const projectName = card.querySelector("h3").innerText;
+        const project = projectData[projectName];
 
-            if (projectData[projectName]) {
-                title.innerText = projectData[projectName].title;
-                desc.innerText = projectData[projectName].description;
-            }
-
-            panel.classList.remove("panel-hide");
-            panel.classList.add("panel-show");
+        if (project && project.url) {
+            window.location.href = project.url;
+        }
         });
     });
 
-    backButton.addEventListener("click", () => {
-        panel.classList.remove("panel-show");
-        panel.classList.add("panel-hide");
-    });
+  backButton.addEventListener("click", () => {
+    panel.classList.remove("panel-show");
+    panel.classList.add("panel-hide");
+  });
 });
